db69e64ff598909c62b1a1515fce03c784f9b249
JohnN05 <johnng0511@gmail.com>
2024-08-05 20:46:53 -0400

    Add animation and icons in Header

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import './Header.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import {faEnvelope} from "@fortawesome/free-regular-svg-icons";
+
+function Header(){
+    return(
+        <nav>
+            <ul>
+                <li><a href="https://linkedin.com/in/johnn05" target="_blank">
+                    <FontAwesomeIcon className="icon" icon={faLinkedin} size="2x" style={{color: "#283618",}} />
+                </a></li>
+                <li><a href="https://github.com/JohnN05" target="_blank">
+                    <FontAwesomeIcon className="icon" icon={faGithub} size="2x" style={{color: "#283618",}} />
+                </a></li>
+                <li><a href = "mailto:jng0511@umd.edu">
+                    <FontAwesomeIcon className="icon" icon={faEnvelope} size="2x" style={{color: "#283618",}} />
+                </a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Header;
